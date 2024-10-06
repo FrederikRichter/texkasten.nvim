aa49c3f0ceb0d92b00cb48d7b741af7f35b35884
Frederik Richter <frederik.richter@mailbox.org>
2024-10-06 20:00:42 +0200

    get current buffer

--- lua/texkasten.lua
+++ lua/texkasten.lua
@@ -1,18 +1,22 @@
+local function log_message(message)
+    vim.cmd('echom("' .. message .. '")')
+end
+
 -- Create a new autocommand group
 local texkasten_group = vim.api.nvim_create_augroup('TexkastenGroup', {clear = true})
 
 local function main(datadir)
-    print("using datadir " .. datadir)
+    log_message("using datadir " .. datadir)
 end
 
 local function setup(datadir)
     main(datadir)
-    vim.api.nvim_create_autocmd({'BufEnter', 'BufRead', 'BufNewFile'}, {
+    vim.api.nvim_create_autocmd({'BufEnter'}, {
         group = texkasten_group,
         pattern = '*/' .. datadir .. '*.tex',  -- Matches .tex files in the texkasten directory
         once = true,
         callback = function()
-            print("Entered a .tex file in the texkasten directory!")
+            log_message("Entered a .tex file in the texkasten directory! Buffer : " .. vim.api.nvim_buf_get_name(0))
             -- You can add more actions here, such as setting options or running commands
         end,
     })
